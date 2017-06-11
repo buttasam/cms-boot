62efff504e0b28f9cde2d40c3bf0fba72e521212
Samuel Butta <samuel@butta.cz>
2017-06-11 23:35:51 +0200

    initial commit

---
+++ src/test/java/front/controller/HomepageControllerTest.java
@@ -0,0 +1,16 @@
+package front.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Samuel Butta
+ */
+public class HomepageControllerTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+}
