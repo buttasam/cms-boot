90f20aa12ebad5bf05b96c6227967d47b615ed56
Samuel Butta <samuel@butta.cz>
2017-06-16 00:56:19 +0200

    add basic UI tests

---
+++ src/test/java/front/controller/HttpRequestTest.java
@@ -0,0 +1,33 @@
+package front.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Samuel Butta
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+    @Test
+    public void test() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("<div style=\"width: 250px; height: 250px; background: black\"></div>");
+    }
+
+}
---
+++ src/test/java/front/controller/SmokeTest.java
@@ -0,0 +1,26 @@
+package front.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Samuel Butta
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmokeTest {
+
+    @Autowired
+    private HomepageController homepageController;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(homepageController).isNotNull();
+    }
+
+}
