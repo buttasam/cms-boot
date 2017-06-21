70c41047e0d5b77825076251ebd208a66d25e92f
Samuel Butta <samuel@butta.cz>
2017-06-21 21:29:48 +0200

    add BeanConfig class

---
+++ src/main/java/app/config/BeanConfig.java
@@ -0,0 +1,21 @@
+package app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Trida definuje pomocne servisni Beans pro IoC kontejner.
+ *
+ * @author Samuel Butta
+ */
+@Configuration
+public class BeanConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
