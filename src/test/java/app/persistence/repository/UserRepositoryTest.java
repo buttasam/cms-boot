5838619d78fdeae897bec315217437706a627546
Samuel Butta <samuel@butta.cz>
2017-06-21 21:22:07 +0200

    update UserRepositoryTest

--- src/test/java/app/persistence/repository/UserRepositoryTest.java
+++ src/test/java/app/persistence/repository/UserRepositoryTest.java
@@ -1,5 +1,7 @@
 package app.persistence.repository;
 
+import app.persistence.entity.Role;
+import app.persistence.entity.RoleType;
 import app.persistence.entity.User;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,6 +13,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.jws.soap.SOAPBinding;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Samuel Butta
@@ -24,21 +28,29 @@ public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
-
     @Test
-    public void testSaveUser() {
+    public void testSaveUserWithRole() {
+
+        Role admin = new Role();
+        admin.setRole(RoleType.ADMIN);
+        Set<Role> roles = new HashSet<>();
+        roles.add(admin);
+
         String email = "sam@jones.cz";
 
         User user = new User();
         user.setFirstName("Sam");
         user.setLastName("Jones");
         user.setEmail(email);
+        user.setRoles(roles);
 
         userRepository.save(user);
 
         User resultUser = userRepository.findByEmail(email);
 
         Assert.assertEquals(email, resultUser.getEmail());
+        Assert.assertEquals(1, resultUser.getRoles().size());
+        Assert.assertEquals(RoleType.ADMIN, resultUser.getRoles().iterator().next().getRole());
     }
 
 
