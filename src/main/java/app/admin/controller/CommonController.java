d1343383c0c5e119515c000a49be13aa6765723c
Samuel Butta <samuel@butta.cz>
2017-07-27 00:09:50 +0200

    change admin menu

--- src/main/java/app/admin/controller/CommonController.java
+++ src/main/java/app/admin/controller/CommonController.java
@@ -1,5 +1,9 @@
 package app.admin.controller;
 
+import app.persistence.entity.auth.User;
+import app.persistence.repository.cms.PageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,9 +16,20 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class CommonController {
 
+    private PageRepository pageRepository;
+
+    @Autowired
+    public CommonController(PageRepository pageRepository) {
+        this.pageRepository = pageRepository;
+    }
+
     @ModelAttribute
     public void addCommonAttributes(Model model) {
+        String loggedUserName = SecurityContextHolder.getContext().getAuthentication().getName();
+
         model.addAttribute("version", "1.0"); // TODO real version number
+        model.addAttribute("pages", pageRepository.findAll());
+        model.addAttribute("loggedUserName", loggedUserName);
     }
 
 }
