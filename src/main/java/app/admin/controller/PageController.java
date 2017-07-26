1cc445af8fde633b4da0da8911228929bed45611
Samuel Butta <samuel@butta.cz>
2017-07-26 23:16:12 +0200

    save page from form

--- src/main/java/app/admin/controller/PageController.java
+++ src/main/java/app/admin/controller/PageController.java
@@ -1,6 +1,8 @@
 package app.admin.controller;
 
 import app.admin.form.PageForm;
+import app.service.cms.PageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,14 @@ import javax.validation.Valid;
 @RequestMapping("/admin")
 public class PageController {
 
+
+    private PageService pageService;
+
+    @Autowired
+    public PageController(PageService pageService) {
+        this.pageService = pageService;
+    }
+
     @RequestMapping("/addPage")
     public String renderDashboard(PageForm pageForm) {
         return "admin/addPage";
@@ -24,8 +34,8 @@ public class PageController {
     @PostMapping("/addPage")
     public String pageForm(@Valid PageForm pageForm, BindingResult bindingResult) {
 
-        if(!bindingResult.hasErrors()) {
-            System.out.println(pageForm.getTitle());
+        if (!bindingResult.hasErrors()) {
+            pageService.savePage(pageForm);
         }
 
         return "redirect:/admin/addPage";
--- src/main/java/app/service/cms/PageService.java
+++ src/main/java/app/service/cms/PageService.java
@@ -1,13 +1,17 @@
 package app.service.cms;
 
+import app.admin.form.PageForm;
+import app.persistence.entity.cms.Page;
 import app.persistence.repository.cms.PageImageRepository;
 import app.persistence.repository.cms.PageRepository;
 import app.persistence.repository.cms.PageTextRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Samuel Butta
  */
+@Service
 public class PageService {
 
 
@@ -20,6 +24,14 @@ public class PageService {
         this.pageRepository = pageRepository;
         this.pageImageRepository = pageImageRepository;
         this.pageTextRepository = pageTextRepository;
+    }
+
+    public void savePage(PageForm pageForm) {
+        Page page = new Page();
+        page.setTitle(pageForm.getTitle());
+        page.setUrl(pageForm.getUrl());
+
+        pageRepository.save(page);
     }
 
 }
