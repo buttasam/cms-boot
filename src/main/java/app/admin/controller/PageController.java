46102c5182523a26064848a20e6b8279d6714697
Samuel Butta <samuel@butta.cz>
2017-07-31 18:54:19 +0200

    add page detail

--- src/main/java/app/admin/controller/PageController.java
+++ src/main/java/app/admin/controller/PageController.java
@@ -1,12 +1,14 @@
 package app.admin.controller;
 
 import app.admin.form.PageForm;
+import app.persistence.entity.cms.Page;
+import app.persistence.repository.cms.PageRepository;
 import app.service.cms.PageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -18,12 +20,23 @@ import javax.validation.Valid;
 public class PageController {
 
 
+    /**
+     * Services
+     */
     private PageService pageService;
 
+
+    /**
+     * Repositories
+     */
+    private PageRepository pageRepository;
+
     @Autowired
-    public PageController(PageService pageService) {
+    public PageController(PageService pageService, PageRepository pageRepository) {
         this.pageService = pageService;
+        this.pageRepository = pageRepository;
     }
+
 
     @RequestMapping("/addPage")
     public String renderDashboard(PageForm pageForm) {
@@ -39,6 +52,18 @@ public class PageController {
         }
 
         return "redirect:/admin/addPage";
+    }
+
+
+    @RequestMapping(value = "/page/{url}")
+    public String renderPage(@PathVariable("url") String url, Model model) {
+
+        // TODO osetrit pokud neexistuje
+        Page page = pageRepository.findByUrl(url);
+
+        model.addAttribute("page", page);
+
+        return "admin/page";
     }
 
 }
