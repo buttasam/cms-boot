6c48613ac70aa39aed573a47752ebbf44419c880
Samuel Butta <samuel@butta.cz>
2018-06-22 11:20:32 +0200

    Add addPageTexts method to front base controller

--- src/main/java/app/front/controller/HomepageController.java
+++ src/main/java/app/front/controller/HomepageController.java
@@ -1,16 +1,9 @@
 package app.front.controller;
 
-import app.common.service.cms.api.PageService;
 import app.config.anotation.FrontController;
 import app.front.controller.parent.FrontAbstractController;
-import app.persistence.entity.cms.Page;
-import app.persistence.repository.cms.PageRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Map;
-import java.util.Optional;
 
 /**
  * @author Samuel Butta
@@ -19,28 +12,9 @@ import java.util.Optional;
 public class HomepageController extends FrontAbstractController {
 
 
-    /**
-     * Repositories
-     */
-    private PageRepository pageRepository;
-
-    /**
-     * Services
-     */
-    private PageService pageService;
-
-    @Autowired
-    public HomepageController(PageRepository pageRepository, PageService pageService) {
-        this.pageRepository = pageRepository;
-        this.pageService = pageService;
-    }
-
-
     @RequestMapping("/")
     public String index(Model model) {
-        Optional<Page> pageOpt = pageRepository.getByUrl("homepage");
-        Map<String, String> pageTexts = pageService.createPageTextsMap(pageOpt);
-        model.addAttribute("pageTexts", pageTexts);
+        addPageTexts(model, "homepage");
 
         return "front/index";
     }
--- src/main/java/app/front/controller/parent/FrontAbstractController.java
+++ src/main/java/app/front/controller/parent/FrontAbstractController.java
@@ -1,10 +1,32 @@
 package app.front.controller.parent;
 
 import app.common.controller.AbstractController;
+import app.common.service.cms.api.PageService;
+import app.persistence.entity.cms.Page;
+import app.persistence.repository.cms.PageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author Samuel Butta
  */
 public class FrontAbstractController extends AbstractController {
+
+
+    @Autowired
+    protected PageRepository pageRepository;
+
+    @Autowired
+    protected PageService pageService;
+
+
+    public void addPageTexts(Model model, String pageIdentity) {
+        Optional<Page> pageOpt = pageRepository.getByUrl(pageIdentity);
+        Map<String, String> pageTexts = pageService.createPageTextsMap(pageOpt);
+        model.addAttribute("pageTexts", pageTexts);
+    }
 
 }
